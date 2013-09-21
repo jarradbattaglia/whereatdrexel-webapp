6cda6edcdd09a7dd84ce450676daaf1312f7efe6
Timothy Hahn <timyhahn@gmail.com>
2013-09-21 11:52:58 -0400

    Added new icons and better searching

--- js/whereatdrexel.js
+++ js/whereatdrexel.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
 
     window.markerLocations = L.layerGroup();
-    var rootURL = "http://localhost:5000"
+    var rootURL = 'http://localhost:5000'
     var map = L.map('map',{
         layers: [markerLocations]
     });
-
-    // Bounds for map
+       // Bounds for map
     var southWest = new L.LatLng(39.948927, -75.215149),
         northEast = new L.LatLng(39.966427, -75.171547),
         bounds = new L.LatLngBounds(southWest, northEast);
     map.fitBounds(bounds);
     map.setMaxBounds(bounds);
     map.panTo([39.957433, -75.189292]);
+    var markerBuilding = L.AwesomeMarkers.icon({
+        icon: 'building', 
+        color: 'darkblue'
+    });
+    var selectedBuilding = L.AwesomeMarkers.icon({
+        icon: 'building', 
+        color: 'blue'
+    });
+    var markerCourse = L.AwesomeMarkers.icon({
+        icon: 'pencil', 
+        color: 'darkblue'
+    });
+    var selectedCourse = L.AwesomeMarkers.icon({
+        icon: 'pencil', 
+        color: 'blue'
+    });
+
+
+    //L.marker([39.957433, -75.189292], {icon: redMarker}).addTo(map);
+
 
     // Tile Layer
     L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg', {
@@ -25,6 +44,8 @@ $(document).ready(function(){
         this.latitude = data.latitude;
         this.longitude = data.longitude;
         this.description = data.description;
+        this.id = data.id;
+        this.type = data.type;
     }
 
     var mapLocations = [];
@@ -33,45 +54,56 @@ $(document).ready(function(){
     function updateMap(){
         markerLocations.clearLayers();
         $.each(mapLocations, function(i, location){
-            L.marker([location.latitude, location.longitude]).addTo(markerLocations)
-                .bindPopup("<h5><strong>" + location.name + "</strong></h5>" + location.description)
+            var marker = markerBuilding;
+            if (location.type == 'course'){
+                marker = markerCourse;
+            }
+            L.marker([location.latitude, location.longitude], {icon: marker}).addTo(markerLocations)
+                .bindPopup('<h5><strong>' + location.name + '</strong></h5>' + location.description)
         });
     }
 
     window.focusOn = function(target){
         markerLocations.clearLayers();
         $.each(mapLocations, function(i, location){
-            if(location.name == target.name) {
-                L.marker([location.latitude, location.longitude]).addTo(markerLocations)
-                .bindPopup("<h5><strong>" + location.name + "</strong></h5>" + location.description).openPopup();
-                map.panTo([location.latitude, location.longitude]);
+            var marker = markerBuilding
+            if (location.type == 'course'){
+                marker = markerCourse;
+            }
+
+            if(location.id == target.id) {
+                if (location.type == 'course'){
+                    marker = selectedCourse;
+                } else {
+                    marker = selectedBuilding;
+                }
+
+                if(target.latitude == 0.00){
+                    $('div#location-info').html(target.description);
+                    $('#info-modal').foundation('reveal', 'open');
+                }
+
+                L.marker([location.latitude, location.longitude], {icon: marker}).addTo(markerLocations)
+                .bindPopup('<h5><strong>' + location.name + '</strong></h5>' + location.description).openPopup();
+                if(target.latitude != 0.00)
+                    map.panTo([location.latitude, location.longitude]);
             }
             else {
-                L.marker([location.latitude, location.longitude]).addTo(markerLocations)
-                    .bindPopup("<h5><strong>" + location.name + "</strong></h5>" + location.description)
+                L.marker([location.latitude, location.longitude], {icon: marker}).addTo(markerLocations)
+                    .bindPopup('<h5><strong>' + location.name + '</strong></h5>' + location.description)
             }
         });
     }
 
-    $.getJSON(rootURL + "/api/buildings", function(data) {
+    $.getJSON(rootURL + '/api/buildings', function(data) {
         cachedLocations = data.locations;
         mapLocations = cachedLocations;
     }).done(function() {
         updateMap();
     });
 
-    // ViewModel for search
-    var searchVM = {
-            searchDrexel : function(formElement) {
-            },
-            
-            searchTerm: ko.observable(),
-            searchResults: ko.observableArray([]),
-    };
-
-    searchVM.searchTerm.subscribe(function(searchValue){
-
-        if (searchValue == "") {
+    window.searchFor = function(searchValue) {
+        if (searchValue == '') {
             searchVM.searchResults.removeAll();
             mapLocations = cachedLocations; 
             updateMap();
@@ -79,8 +111,7 @@ $(document).ready(function(){
         };
 
         $.ajax({
-            url: rootURL + "/api/search/" + searchValue,
-            //dataType: 'json', 
+            url: rootURL + '/api/search/' + searchValue,
             success: function(data){
                 searchVM.searchResults.removeAll();
                  $.each(data.locations, function(i, location) {
@@ -92,6 +123,20 @@ $(document).ready(function(){
             },
             error: function (xhr, textStatus, errorThrown) { console.log('error ' + (errorThrown ? errorThrown : xhr.status) + textStatus); }
         });
+
+    }
+    // ViewModel for search
+    var searchVM = {
+            searchDrexel : function(formElement) {
+                searchFor(searchVM.searchTerm());
+            },
+            
+            searchTerm: ko.observable().extend({ throttle: 500 }),
+            searchResults: ko.observableArray([]),
+    };
+
+    searchVM.searchTerm.subscribe(function(searchValue){
+        searchFor(searchValue);
     });
 
     ko.applyBindings(searchVM); 
